3ea1dd4bdef8c530b05b9f145701b15daffff7f7
likesh50 <like22050.it@rmkec.ac.in>
2024-07-23 15:27:27 +0530

    Sidebar update

--- FrontEnd/MessManagement/src/Components/Navigation.jsx
+++ FrontEnd/MessManagement/src/Components/Navigation.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Navigation.css'
 function Navigation() {
   return (
-    <div>Navigation</div>
+    <div className='navigation'>Navigation</div>
   )
 }
 
--- FrontEnd/MessManagement/src/Components/SideBar.jsx
+++ FrontEnd/MessManagement/src/Components/SideBar.jsx
@@ -1,35 +1,35 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './SideBar.css';
 import { Link, useLocation } from 'react-router-dom';
-
-
+import front from '../assets/Front.jpg';
+import purchase from '../assets/purchase.png'
+import dispatch from '../assets/dispatch.png'
+import Available from '../assets/Available.png'
 function SideBar() {
 
-
+    const isActive = (path) => {
+        return location.pathname === path ? 'active' : '';
+      };
   return (
     <div className="sidebar">
       <ul>
-        <li >
-          
-            Dashboard
+      <li className={isActive('/purchase')}>
+          <Link to="purchase">
+            <img src={purchase} width="40px" height="40px" alt="Faculty Details" />
+            Purchase
+          </Link>
         </li>
-      </ul>
-      <ul>
-        <li >
-          
-            Dashboard
+        <li className={isActive('/purchase')}>
+          <Link to="purchase">
+            <img src={dispatch} width="60px" height="40px" alt="Mail" />
+            Dispatch
+          </Link>
         </li>
-      </ul>
-      <ul>
-        <li >
-          
-            Dashboard
-        </li>
-      </ul>
-      <ul>
-        <li >
-          
-            Dashboard
+        <li className={isActive('/purchase')}>
+          <Link to="purchase">
+            <img src={Available} width="40px" height="40px" alt="other forms" />
+            Available Stock
+          </Link>
         </li>
       </ul>
     </div>
