38dc41c5f6f880819be97037746d5b60d624fe6f
likesh50 <like22050.it@rmkec.ac.in>
2024-09-27 10:45:17 +0530

    deployment changes

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',     
   user: 'root',        
-  password: 'pass123',  
+  password: '1207',  
   database: 'food'  
 });
 
